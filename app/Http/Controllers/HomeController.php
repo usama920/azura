8857acdf3b45abbd3df225fbb15c67d99ce20acb
Usama Aslam <awesomeusama920@gmail.com>
2023-12-23 20:36:29 +0500

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,35 +10,7 @@ use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
-    public function LoginPage()
+    public function Dashboard()
     {
-        if (Auth::check()) {
-            return redirect('/dashboard');
-        }
-        return view('login');
-    }
-
-    public function TryLogin(Request $request)
-    {
-        $request->validate([
-            'username' => 'required',
-            'password' => 'required'
-        ]);
-
-        $credentials = ["username" => $request->username, "password" => $request->password];
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect('/dashboard');
-        } else {
-            Session::flash('message', 'Invalid Credentials');
-            Session::flash('alert-type', 'error');
-            return redirect()->back();
-        }
-    }
-
-    public function Logout()
-    {
-        Auth::logout();
-        return redirect('/login');
     }
 }
