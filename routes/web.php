4fe82946915a8c78c2577e0a60071b6e97784cc9
Usama Aslam <awesomeusama920@gmail.com>
2023-12-23 20:35:35 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [LoginController::class, 'LoginPage']);
 Route::get('/login', [LoginController::class, 'LoginPage']);
 Route::post('/tryLogin', [LoginController::class, 'TryLogin']);
 Route::post('/logout', [LoginController::class, 'Logout']);
+
+Route::group(['prefix' => '/', 'middleware' => 'user_auth'], function () {
+    Route::get('/dashboard', [HomeController::class, 'Dashboard']);
+});
