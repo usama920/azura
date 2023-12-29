12c2cbf5344c67083020efa30cd2cc150ff219c4
Usama Aslam <awesomeusama920@gmail.com>
2023-12-29 19:00:33 +0500

    new

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,65 +12,41 @@ class LoginController extends Controller
 {
     public function LoginPage()
     {
-        $whmcsUrl = "https://portal.casthost.net/";
-        $api_identifier = "s1xS6qAtUE0z5HoYdFVnE2wO3HXfHQ2g";
-        $api_secret = "XzBYD7151kGeMaU7ApoFKkLgXZKEc9QC";
+        // $whmcsUrl = "https://portal.casthost.net/";
+        // $api_identifier = "s1xS6qAtUE0z5HoYdFVnE2wO3HXfHQ2g";
+        // $api_secret = "XzBYD7151kGeMaU7ApoFKkLgXZKEc9QC";
 
-        // Set post values
-        $postfields = array(
-            'identifier' => 'Usama',
-            'secret' => md5('TempPass12!'),
-            'action' => 'GetStats',
-            'responsetype' => 'json',
-        );
+        // // Set post values
+        // $postfields = array(
+        //     'identifier' => 'Usama',
+        //     'secret' => md5('TempPass12!'),
+        //     'action' => 'GetStats',
+        //     'responsetype' => 'json',
+        // );
 
-        // Call the API
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $whmcsUrl . 'includes/api.php');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
-        $response = curl_exec($ch);
-        // if (curl_error($ch)) {
-        //     die('Unable to connect: ' . curl_errno($ch) . ' - ' . curl_error($ch));
-        // }
-        curl_close($ch);
+        // // Call the API
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $whmcsUrl . 'includes/api.php');
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+        // $response = curl_exec($ch);
+        // // if (curl_error($ch)) {
+        // //     die('Unable to connect: ' . curl_errno($ch) . ' - ' . curl_error($ch));
+        // // }
+        // curl_close($ch);
 
-        // Decode response
-        // $jsonData = json_decode($response, true);
+        // // Decode response
+        // // $jsonData = json_decode($response, true);
 
-        // Dump array structure for inspection
-        // var_dump($jsonData);
-        echo "<pre>";
-        print_r($response);
-        die;
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://portal.casthost.net/includes/api.php');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt(
-            $ch,
-            CURLOPT_POSTFIELDS,
-            http_build_query(
-                array(
-                    'action' => 'GetProducts',
-                    'username' => 's1xS6qAtUE0z5HoYdFVnE2wO3HXfHQ2g',
-                    'password' => 'XzBYD7151kGeMaU7ApoFKkLgXZKEc9QC',
-                    'pid' => '81',
-                    'responsetype' => 'json',
-                )
-            )
-        );
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        echo "<pre>";
-        print_r($response);
-        die;
+        // // Dump array structure for inspection
+        // // var_dump($jsonData);
+        // echo "<pre>";
+        // print_r($response);
+        // die;
 
         if (Auth::check()) {
             return redirect('/dashboard');
